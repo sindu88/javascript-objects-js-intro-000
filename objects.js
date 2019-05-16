5f0dcdd12082e94f62b2ecbb6fc2916d15f90b1f
sinduja <sindhu.38@gmail.com>
2019-05-16 03:50:25 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,7 @@
 var  playlist= { Rahman: 'Urvasi' }
 function updatePlaylist(playlist,artistName,songTitle){
-playlist= { artistName: [songTitle] };
+Object.assign({}, playlist, { [artistName]: [songTitle]})
+
 }
 function removeFromPlaylist(playlist,artistName)
 {
